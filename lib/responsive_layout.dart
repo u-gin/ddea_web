358d4dbfed7874f50d222ea8a13b863730f0050d
Eugene Asiedu <uginasiedu079@gmail.com>
2023-08-24 02:27:20 +0000

    fixed constraint sizes for tablet mode

--- lib/responsive_layout.dart
+++ lib/responsive_layout.dart
@@ -15,9 +15,9 @@ class ResponsiveLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.minWidth < 450) {
+      if (constraints.maxWidth <= 500) {
         return mobileLayout;
-      } else if (constraints.minWidth < 110) {
+      } else if (constraints.maxWidth < 1100) {
         return tabletLayout;
       } else {
         return desktopLayout;
