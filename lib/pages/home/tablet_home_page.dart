3581311e283e0d371ac56200be8a7747c6f6c183
Eugene Asiedu <uginasiedu079@gmail.com>
2023-08-25 00:26:52 +0000

    Added a stateful widget

---
+++ lib/pages/home/tablet_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabletHomePage extends StatefulWidget {
+  const TabletHomePage({super.key});
+
+  @override
+  State<TabletHomePage> createState() => _TabletHomePageState();
+}
+
+class _TabletHomePageState extends State<TabletHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
