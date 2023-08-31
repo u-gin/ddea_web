8df80e933005ebd39329cfae85449a8dd580df09
Eugene Asiedu <uginasiedu079@gmail.com>
2023-08-31 02:35:53 +0000

    Add constants file to hold constant functions and widgets

---
+++ lib/pages/religious/constants.dart
@@ -0,0 +1,30 @@
+List<String> shepherdList = [
+  "Eld. Godfrey Kofi Saim",
+  "Eld. Francis Kofi Annor",
+  "Eld. Felix Kwame Ametor",
+  "Eld. Isaac Amoh",
+  "Eld. Ernest Kofi Antwi",
+  "Eld. Richard Siaw",
+  "Eld. Gordon Owusu Mensah",
+  "Eld. Charles Ampofo",
+  "Dcns. Dorothy Delali Acheampong",
+  "Dcns. Leticia Aidoo",
+  "Dcns. Grace Obeng",
+  "Dcns. Grace Antwi",
+  "Dcns. Cynthia Asare",
+  "Dcns. Deborah Asiedu",
+  "Dcns. Mercy Abeyie",
+  "Dcn. Charles Hadjor",
+  "Eld. Isaac Fiakpornu",
+  "Dcns. Abigail Deedei Adu",
+  "Deacon Mark Ekow Tawiah",
+  "Eld. Jeremiah Dzorkpe-Yevenyo",
+  "Dcn. Daniel Annor Boateng"
+];
+
+List positionHeldList = [
+  'Member (M)',
+  'Presiding Elder (P.E)',
+  'Deacon (Dcn)',
+  'Deaconess (Dcns)'
+];
