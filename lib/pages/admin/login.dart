a38f4515eabe6e4be652e6d19d9c825e42c36a84
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-02 03:14:09 +0000

    Added admin login page

---
+++ lib/pages/admin/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SizedBox(
+        child: Center(
+          child: Text("Admin Login Page"),
+        ),
+      ),
+    );
+  }
+}
