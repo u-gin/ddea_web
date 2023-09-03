f41a7dcc6aed9612b9d74eec5b3772a1417d9577
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-03 23:46:04 +0000

    Building the admin login page, added the input box

--- lib/pages/admin/login_page.dart
+++ lib/pages/admin/login_page.dart
@@ -1,14 +1,72 @@
+import 'package:ddea_web/utils/colors.dart';
+import 'package:ddea_web/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+import '../../widgets/text_field_template.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController passcodeController;
+
+  @override
+  void initState() {
+    passcodeController = TextEditingController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SizedBox(
-        child: Center(
-          child: Text("Admin Login Page"),
+        width: canvasWidth,
+        height: canvasHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "ADMIN LOGIN PAGE",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Enter passcode",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                TextFieldTemplate(
+                  hintText: "***********",
+                  controller: passcodeController,
+                  obscureText: true,
+                  height: 50,
+                  textInputType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.next,
+                  enabled: true,
+                  textFieldColor: AppColors.primary,
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
