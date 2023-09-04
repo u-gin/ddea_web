d01abb4e70225dc0bd6a94c196cc6d1e0d8227f6
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-04 00:58:18 +0000

    Removed unused import

--- lib/pages/admin/login_page.dart
+++ lib/pages/admin/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:ddea_web/utils/colors.dart';
 import 'package:ddea_web/utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
