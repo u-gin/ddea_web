f972b73ecfa801eb0f288ffab56450eb65246953
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-04 00:57:44 +0000

    Added firebase sign in method

--- lib/pages/admin/login_page.dart
+++ lib/pages/admin/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:ddea_web/utils/colors.dart';
 import 'package:ddea_web/utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/text_field_template.dart';
@@ -13,10 +14,12 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   late TextEditingController passcodeController;
+  late TextEditingController emailController;
 
   @override
   void initState() {
     passcodeController = TextEditingController();
+    emailController = TextEditingController();
     super.initState();
   }
 
@@ -46,7 +49,29 @@ class _LoginPageState extends State<LoginPage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
-                  "Enter passcode",
+                  "Email address",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                TextFieldTemplate(
+                  hintText: "abc@gmail.com",
+                  controller: emailController,
+                  obscureText: false,
+                  height: 50,
+                  textInputType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  enabled: true,
+                  textFieldOutlineColor: Colors.deepPurple,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Password",
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 16.0,
@@ -60,7 +85,7 @@ class _LoginPageState extends State<LoginPage> {
                   obscureText: true,
                   height: 50,
                   textInputType: TextInputType.visiblePassword,
-                  textInputAction: TextInputAction.next,
+                  textInputAction: TextInputAction.go,
                   enabled: true,
                   textFieldOutlineColor: Colors.deepPurple,
                 )
@@ -70,5 +95,21 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = userCredential.user;
+      return user;
+    } catch (e) {
+      debugPrint("Sign-in error: $e");
+      return null;
+    }
   }
 }
