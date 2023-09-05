164090fd9d8059089c154d699f328a81871cf22f
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-05 02:08:51 +0000

    Modified user list item template to look cleaner

--- lib/pages/admin/user_list_template.dart
+++ lib/pages/admin/user_list_template.dart
@@ -5,25 +5,31 @@ import 'package:ddea_web/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class UserListTemplate extends StatelessWidget {
-  final Uint8List imageBytes;
-  final String fullName, telephone;
+  final Uint8List? imageBytes;
+  final String fullName, telephone, dateOfBirth, dateJoined;
 
   const UserListTemplate({
     Key? key,
-    required this.imageBytes,
+    this.imageBytes,
     required this.fullName,
     required this.telephone,
+    required this.dateOfBirth,
+    required this.dateJoined,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
+    return SizedBox(
+      width: canvasWidth,
       child: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              ClipOval(
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /* ClipOval(
                 child: Container(
                   width: 80,
                   height: 80,
@@ -42,35 +48,66 @@ class UserListTemplate extends StatelessWidget {
                     ),
                   ),
                 ),
-              ),
-              Text(
-                fullName,
-                style: const TextStyle(
-                  fontSize: 17.0,
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.w500,
-                ),
+              ), */
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey[300],
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      color: Colors.grey,
+                      size: 30,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Text(
+                    fullName,
+                    style: textStyle(),
+                  ),
+                ],
               ),
               Text(
                 telephone,
-                style: const TextStyle(
-                  fontSize: 17.0,
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.w500,
-                ),
+                style: textStyle(),
               ),
+              Text(
+                dateOfBirth,
+                style: textStyle(),
+              ),
+              Text(
+                dateJoined,
+                style: textStyle(),
+              )
             ],
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
             child: Container(
               height: 1,
               width: canvasWidth,
-              color: Colors.black,
+              //color: Colors.grey[200],
+              color: Colors.grey[50],
             ),
           )
         ],
       ),
+    );
+  }
+
+  TextStyle textStyle() {
+    return const TextStyle(
+      fontSize: 15.0,
+      fontFamily: "Poppins",
+      fontWeight: FontWeight.w500,
     );
   }
 }
