cfd35901545ac47138f936575be8a04f69343b0e
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-05 03:03:59 +0000

    Added header to list

--- lib/pages/admin/dashboard.dart
+++ lib/pages/admin/dashboard.dart
@@ -19,20 +19,74 @@ class Dashboard extends StatelessWidget {
             ),
             Expanded(
               flex: 7,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 100),
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return const UserListTemplate(
-                      fullName: "fullName",
-                      telephone: "telephone",
-                      dateOfBirth: "10.11.2023",
-                      dateJoined: "10.11.2023",
-                      connectGroup: "Elohim",
-                    );
-                  },
+              child: SizedBox(
+                height: canvasHeight,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 100),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 50,
+                              ),
+                              const SizedBox(
+                                width: 50,
+                              ),
+                              Text(
+                                "Full name",
+                                style: headerTextStyle(),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Telephone",
+                            style: headerTextStyle(),
+                          ),
+                          Text(
+                            "Date of birth",
+                            style: headerTextStyle(),
+                          ),
+                          Text(
+                            "Date joined",
+                            style: headerTextStyle(),
+                          ),
+                          Text(
+                            "Connect group",
+                            style: headerTextStyle(),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20, left: 200, right: 100),
+                        child: Container(
+                          height: 1,
+                          width: canvasWidth,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 8,
+                        child: ListView.builder(
+                          itemCount: 90,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return const UserListTemplate(
+                              fullName: "fullName",
+                              telephone: "telephone",
+                              dateOfBirth: "10.11.2023",
+                              dateJoined: "10.11.2023",
+                              connectGroup: "Elohim",
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
