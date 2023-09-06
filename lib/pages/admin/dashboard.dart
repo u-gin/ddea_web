238d5bdbeb4de645a34fd90c2be1290dbbcfd729
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-06 23:28:26 +0000

    Modified dashboard

--- lib/pages/admin/dashboard.dart
+++ lib/pages/admin/dashboard.dart
@@ -3,94 +3,84 @@ import 'package:ddea_web/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[10],
       body: SizedBox(
         width: canvasWidth,
         height: canvasHeight,
-        child: Row(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Container(
-                color: Colors.lightBlueAccent,
-              ),
-            ),
-            Expanded(
-              flex: 7,
-              child: SizedBox(
-                height: canvasHeight,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 100),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Row(
-                            children: [
-                              const SizedBox(
-                                width: 50,
-                              ),
-                              const SizedBox(
-                                width: 50,
-                              ),
-                              Text(
-                                "Full name",
-                                style: headerTextStyle(),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            "Telephone",
-                            style: headerTextStyle(),
-                          ),
-                          Text(
-                            "Date of birth",
-                            style: headerTextStyle(),
-                          ),
-                          Text(
-                            "Date joined",
-                            style: headerTextStyle(),
-                          ),
-                          Text(
-                            "Connect group",
-                            style: headerTextStyle(),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 20.0, bottom: 20, left: 200, right: 100),
-                        child: Container(
-                          height: 1,
-                          width: canvasWidth,
-                          color: Colors.grey[400],
+        child: SizedBox(
+          height: canvasHeight,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 50,
                         ),
-                      ),
-                      Expanded(
-                        flex: 8,
-                        child: ListView.builder(
-                          itemCount: 90,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return const UserListTemplate(
-                              fullName: "fullName",
-                              telephone: "telephone",
-                              dateOfBirth: "10.11.2023",
-                              dateJoined: "10.11.2023",
-                              connectGroup: "Elohim",
-                            );
-                          },
+                        const SizedBox(
+                          width: 50,
                         ),
-                      ),
-                    ],
+                        Text(
+                          "Full name",
+                          style: headerTextStyle(),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Telephone",
+                      style: headerTextStyle(),
+                    ),
+                    Text(
+                      "Date of birth",
+                      style: headerTextStyle(),
+                    ),
+                    Text(
+                      "Date joined",
+                      style: headerTextStyle(),
+                    ),
+                    Text(
+                      "Connect group",
+                      style: headerTextStyle(),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 200),
+                  child: Container(
+                    height: 1,
+                    width: canvasWidth,
+                    color: Colors.grey[400],
                   ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: ListView.builder(
+                    itemCount: 90,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return const UserListTemplate(
+                        fullName: "fullName",
+                        telephone: "telephone",
+                        dateOfBirth: "10.11.2023",
+                        dateJoined: "10.11.2023",
+                        connectGroup: "Elohim",
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
