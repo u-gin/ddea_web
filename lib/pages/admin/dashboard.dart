c302a4afbcf931905ffe10b0ed66a5c99a2d0e27
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-06 23:51:25 +0000

    Added search box tto filter list

--- lib/pages/admin/dashboard.dart
+++ lib/pages/admin/dashboard.dart
@@ -1,85 +1,137 @@
 import 'package:ddea_web/pages/admin/user_list_template.dart';
 import 'package:ddea_web/utils/constants.dart';
+import 'package:ddea_web/widgets/text_field_template.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  late TextEditingController nameController;
+
+  @override
+  void initState() {
+    nameController = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[10],
       body: SizedBox(
-        width: canvasWidth,
         height: canvasHeight,
-        child: SizedBox(
-          height: canvasHeight,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 100),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        width: canvasWidth,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 200.0),
+                child: Row(
                   children: [
                     Row(
                       children: [
-                        const SizedBox(
-                          width: 50,
+                        TextFieldTemplate(
+                          hintText: "Search name",
+                          controller: nameController,
+                          obscureText: false,
+                          height: 50,
+                          width: 300,
+                          textFieldOutlineColor: Colors.deepPurple,
+                          textInputType: TextInputType.name,
+                          textInputAction: TextInputAction.search,
+                          enabled: true,
                         ),
                         const SizedBox(
-                          width: 50,
+                          width: 5,
                         ),
-                        Text(
-                          "Full name",
-                          style: headerTextStyle(),
-                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.deepPurple,
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                    Text(
-                      "Telephone",
-                      style: headerTextStyle(),
-                    ),
-                    Text(
-                      "Date of birth",
-                      style: headerTextStyle(),
-                    ),
-                    Text(
-                      "Date joined",
-                      style: headerTextStyle(),
-                    ),
-                    Text(
-                      "Connect group",
-                      style: headerTextStyle(),
-                    )
                   ],
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 200),
-                  child: Container(
-                    height: 1,
-                    width: canvasWidth,
-                    color: Colors.grey[400],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 50,
+                      ),
+                      const SizedBox(
+                        width: 50,
+                      ),
+                      Text(
+                        "Full name",
+                        style: headerTextStyle(),
+                      ),
+                    ],
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: ListView.builder(
-                    itemCount: 90,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return const UserListTemplate(
-                        fullName: "fullName",
-                        telephone: "telephone",
-                        dateOfBirth: "10.11.2023",
-                        dateJoined: "10.11.2023",
-                        connectGroup: "Elohim",
-                      );
-                    },
+                  Text(
+                    "Telephone",
+                    style: headerTextStyle(),
                   ),
+                  Text(
+                    "Date of birth",
+                    style: headerTextStyle(),
+                  ),
+                  Text(
+                    "Date joined",
+                    style: headerTextStyle(),
+                  ),
+                  Text(
+                    "Connect group",
+                    style: headerTextStyle(),
+                  )
+                ],
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 200),
+                child: Container(
+                  height: 1,
+                  width: canvasWidth,
+                  color: Colors.grey[400],
                 ),
-              ],
-            ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ListView.builder(
+                  itemCount: 90,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return const UserListTemplate(
+                      fullName: "fullName",
+                      telephone: "telephone",
+                      dateOfBirth: "10.11.2023",
+                      dateJoined: "10.11.2023",
+                      connectGroup: "Elohim",
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
