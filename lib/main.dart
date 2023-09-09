616cf18da6a2fb5b8665cbd939971d1c26a47799
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-09 01:20:17 +0000

    Modified code

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:ddea_web/pages/landing/desktop_landing_page.dart';
 import 'package:ddea_web/pages/landing/mobile_landing_page.dart';
 import 'package:ddea_web/pages/landing/tablet_landing_page.dart';
 import 'package:ddea_web/pages/personal/desktop_personal_details_page.dart';
+import 'package:ddea_web/pages/religious/religious_details_page.dart';
 import 'package:ddea_web/responsive_layout.dart';
 import 'package:ddea_web/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -41,6 +42,7 @@ class MainApp extends StatelessWidget {
             ),
         '/home': (context) => const DesktopHomePage(),
         '/personalDetailsPage': (context) => const DesktopPersonalDetailsPage(),
+        '/religiousDetail': (context) => const ReligiousDetailsPage(),
         '/dashboard': (context) => const Dashboard(),
       },
     );
