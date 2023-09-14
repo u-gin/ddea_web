12e77e9a9e4096ebae77b7dc230be8ca5cf8b945
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-14 17:02:20 +0000

    Added another page to display full member details

---
+++ lib/pages/admin/member_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MemberDetailsPage extends StatelessWidget {
+  const MemberDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
