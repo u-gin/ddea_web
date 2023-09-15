2be13b55a283e0d78d13f71f175c19e35bffab2a
Eugene Asiedu <uginasiedu079@gmail.com>
2023-09-15 02:14:34 +0000

    Added member image, name and mobile number to page

--- lib/pages/admin/member_details_page.dart
+++ lib/pages/admin/member_details_page.dart
@@ -1,7 +1,15 @@
+import 'dart:typed_data';
+
+import 'package:ddea_web/models/user_model.dart';
+import 'package:ddea_web/utils/colors.dart';
+import 'package:ddea_web/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MemberDetailsPage extends StatelessWidget {
-  const MemberDetailsPage({super.key});
+  MemberDetailsPage({super.key});
+
+  final List member = Get.arguments;
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +18,78 @@ class MemberDetailsPage extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              flex: 3,
+              flex: 4,
               child: Container(
-                color: Colors.purple,
+                height: canvasHeight,
+                color: AppColors.lightGray,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FutureBuilder<Uint8List?>(
+                      future: member[1],
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          final Uint8List? url = snapshot.data;
+                          if (url != null) {
+                            return ClipOval(
+                              child: Container(
+                                width: 150,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: MemoryImage(
+                                      url,
+                                    ),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            );
+                          } else {
+                            return const Icon(
+                              Icons.person,
+                              color: Colors.grey,
+                              size: 30,
+                            );
+                          }
+                        } else if (snapshot.hasError) {
+                          return const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                            size: 20,
+                          );
+                        } else {
+                          // The Future is still loading.
+                          return const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                              color: Colors.deepPurple,
+                            ),
+                          ); // You can use any loading indicator.
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      member[0].fullName.toString().toUpperCase(),
+                      style: memberDetailsHeaderTextStyle(),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      member[0].telephone,
+                      style: memberDetailsTextStyle(),
+                    )
+                  ],
+                ),
               ),
             ),
             Expanded(
-              flex: 7,
+              flex: 6,
               child: Container(
                 color: Colors.white,
               ),
@@ -27,3 +100,25 @@ class MemberDetailsPage extends StatelessWidget {
     );
   }
 }
+
+/* 
+ String? fullName;
+  String? telephone;
+  String? gender;
+  String? dateOfBirth;
+  String? hometown;
+  String? placeOfResidence;
+  String? residentialAddress;
+  String? placeOfWork;
+  String? occupation;
+  String? baptizedBy;
+  String? communicant;
+  String? positionHeld;
+  String? baptismType;
+  String? connectGroup;
+  String? dateAdded;
+  String? ministry;
+  String? placeOfBirth;
+  String? shepherd;
+  String? timeAdded;
+ */
