706b9c78bb6606f0b043cc001e5e9162015631b1
Eugene Asiedu <uginasiedu079@gmail.com>
2023-12-03 00:52:25 +0000

    Added a requests page to manage all sign up requests

---
+++ lib/pages/admin/requests_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RequestsPage extends StatefulWidget {
+  const RequestsPage({super.key});
+  static const String routeName = "requests";
+
+  @override
+  State<RequestsPage> createState() => _RequestsPageState();
+}
+
+class _RequestsPageState extends State<RequestsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
