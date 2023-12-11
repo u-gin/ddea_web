c5fa9ef72b09e5736601ddaa4df6f451182da690
Eugene Asiedu <uginasiedu079@gmail.com>
2023-12-11 20:58:08 +0000

    Desiging requeest page layout

---
+++ lib/pages/admin/view_requests_layout.dart
@@ -0,0 +1,23 @@
+import 'package:ddea_web/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class ViewRequestLayout extends StatefulWidget {
+  const ViewRequestLayout({super.key});
+
+  @override
+  State<ViewRequestLayout> createState() => _ViewRequestLayoutState();
+}
+
+class _ViewRequestLayoutState extends State<ViewRequestLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: canvasWidth,
+      child: Column(
+        children: [
+          Text(''),
+        ],
+      ),
+    );
+  }
+}
