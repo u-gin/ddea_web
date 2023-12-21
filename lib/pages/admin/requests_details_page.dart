40107a9478440bc20099cc117b2e7269c68a0e20
Eugene Asiedu <uginasiedu079@gmail.com>
2023-12-21 00:54:50 +0000

    Added provider and consumer widget

--- lib/pages/admin/requests_details_page.dart
+++ lib/pages/admin/requests_details_page.dart
@@ -1,11 +1,15 @@
 import 'dart:typed_data';
 
+import 'package:ddea_web/helpers/firebase_provider.dart';
+import 'package:ddea_web/pages/admin/requests_page.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import '../../utils/colors.dart';
 import '../../utils/constants.dart';
+import 'dashboard.dart';
 
 class RequestsDetailsPage extends StatefulWidget {
   const RequestsDetailsPage({super.key});
@@ -20,7 +24,8 @@ class _RequestsDetailsPageState extends State<RequestsDetailsPage> {
   final FirebaseDatabase database = FirebaseDatabase.instance;
 
   final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
-  bool isLoading = false;
+  /* bool isLoadingApproved = false;
+  bool isLoadingDisapproved = false; */
 
   @override
   Widget build(BuildContext context) {
@@ -32,159 +37,168 @@ class _RequestsDetailsPageState extends State<RequestsDetailsPage> {
             Expanded(
               flex: 4,
               child: Container(
-                height: canvasHeight,
-                color: AppColors.lightGray,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FutureBuilder<Uint8List?>(
-                      future: member[1],
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          final Uint8List? url = snapshot.data;
-                          if (url != null) {
-                            return CircleAvatar(
-                              radius: 150,
-                              backgroundImage: MemoryImage(url),
-                              child: ClipOval(
-                                child: Image.memory(
-                                  url,
-                                  fit: BoxFit.cover,
+                  height: canvasHeight,
+                  color: AppColors.lightGray,
+                  child: Consumer<FirebaseProvider>(
+                    builder: (context, firebase, child) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FutureBuilder<Uint8List?>(
+                            future: member[1],
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.done) {
+                                final Uint8List? url = snapshot.data;
+                                if (url != null) {
+                                  return CircleAvatar(
+                                    radius: 150,
+                                    backgroundImage: MemoryImage(url),
+                                    child: ClipOval(
+                                      child: Image.memory(
+                                        url,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return const Icon(
+                                    Icons.person,
+                                    color: Colors.grey,
+                                    size: 30,
+                                  );
+                                }
+                              } else if (snapshot.hasError) {
+                                return const Icon(
+                                  Icons.error,
+                                  color: Colors.red,
+                                  size: 20,
+                                );
+                              } else {
+                                // The Future is still loading.
+                                return const Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2,
+                                    color: Colors.deepPurple,
+                                  ),
+                                ); // You can use any loading indicator.
+                              }
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            member[0].fullName.toString().toUpperCase(),
+                            style: memberDetailsHeaderTextStyle(),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            member[0].telephone,
+                            style: memberDetailsTextStyle(),
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  firebase.isLoadingDelete = true;
+                                  firebase.deleteRequest(member[0].positionHeld,
+                                      member[0].id, (success) => {});
+                                  firebase.retrieveAllUsers();
+                                  firebase.retrieveRequests();
+                                  Get.offAll(() => const Dashboard());
+                                },
+                                child: Container(
+                                  width: 140,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.redAccent,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Center(
+                                      child: firebase.isLoadingDelete
+                                          ? SizedBox(
+                                              height: 16,
+                                              width: 16,
+                                              child: CircularProgressIndicator(
+                                                backgroundColor:
+                                                    AppColors.white,
+                                                strokeWidth: 1,
+                                              ),
+                                            )
+                                          : const Text(
+                                              'Disapprove',
+                                              style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            );
-                          } else {
-                            return const Icon(
-                              Icons.person,
-                              color: Colors.grey,
-                              size: 30,
-                            );
-                          }
-                        } else if (snapshot.hasError) {
-                          return const Icon(
-                            Icons.error,
-                            color: Colors.red,
-                            size: 20,
-                          );
-                        } else {
-                          // The Future is still loading.
-                          return const Padding(
-                            padding: EdgeInsets.all(10.0),
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                              color: Colors.deepPurple,
-                            ),
-                          ); // You can use any loading indicator.
-                        }
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      member[0].fullName.toString().toUpperCase(),
-                      style: memberDetailsHeaderTextStyle(),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      member[0].telephone,
-                      style: memberDetailsTextStyle(),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isLoading = true;
-                            });
-                          },
-                          child: Container(
-                            width: 140,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.redAccent,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Center(
-                                child: isLoading
-                                    ? SizedBox(
-                                        height: 16,
-                                        width: 16,
-                                        child: CircularProgressIndicator(
-                                          backgroundColor: AppColors.white,
-                                          strokeWidth: 1,
-                                        ),
-                                      )
-                                    : const Text(
-                                        'Disapprove',
-                                        style: TextStyle(
-                                          fontSize: 14.0,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
+                              const SizedBox(
+                                width: 10,
                               ),
-                            ),
+                              GestureDetector(
+                                onTap: () {
+                                  firebase.isLoadingMove = true;
+                                  firebase.moveRequestsToApproved(
+                                      member[0].positionHeld,
+                                      member[0].id,
+                                      (success) => {});
+                                  firebase.retrieveAllUsers();
+                                  firebase.retrieveRequests();
+                                  Get.offAll(() => const Dashboard());
+                                },
+                                child: Container(
+                                  width: 140,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.deepPurple,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                    child: Center(
+                                      child: firebase.isLoadingMove
+                                          ? SizedBox(
+                                              height: 16,
+                                              width: 16,
+                                              child: CircularProgressIndicator(
+                                                backgroundColor:
+                                                    AppColors.white,
+                                                strokeWidth: 1,
+                                              ),
+                                            )
+                                          : const Text(
+                                              'Approve',
+                                              style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              isLoading = true;
-                            });
-                            moveToApprovedList((success) {
-                              if (success) {
-                                Get.back();
-                              } else {
-                                Get.back();
-                              }
-                            });
-                          },
-                          child: Container(
-                            width: 140,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.deepPurple,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10),
-                              child: Center(
-                                child: isLoading
-                                    ? SizedBox(
-                                        height: 16,
-                                        width: 16,
-                                        child: CircularProgressIndicator(
-                                          backgroundColor: AppColors.white,
-                                          strokeWidth: 1,
-                                        ),
-                                      )
-                                    : const Text(
-                                        'Approve',
-                                        style: TextStyle(
-                                          fontSize: 14.0,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+                        ],
+                      );
+                    },
+                  )),
             ),
             Expanded(
               flex: 6,
@@ -385,47 +399,37 @@ class _RequestsDetailsPageState extends State<RequestsDetailsPage> {
     );
   }
 
-  moveToApprovedList(Function(bool success) callback) async {
+  /* deleteEntry(Function(bool success) callback, String key) async {
     final databaseReference =
-        database.ref("ddea/approved/members/${userDetails["positionHeld"]}");
+        database.ref("ddea/members/${member[0].positionHeld}/$key");
 
-    await databaseReference.push().set({
-      "Fullname": member[0].fullName,
-      "Mobile number": member[0].telephone,
-      "Place of birth": member[0].placeOfBirth,
-      "Hometown": member[0].hometown,
-      "Gender": member[0].gender,
-      "Date of birth": member[0].dateOfBirth,
-      "Place of residence": member[0].placeOfResidence,
-      "Residential address": member[0].residentialAddress,
-      "Profession": member[0].profession,
-      "Place of work": member[0].placeOfWork,
-      "Baptized by": member[0].baptizedBy,
-      "Position held": member[0].positionHeld,
-      "Communicant": member[0].communicant,
-      "Shepherd": member[0].shepherd,
-      "Baptism received": member[0].baptismType,
-      "Connect group": member[0].connectGroup,
-      "Ministry": member[0].connectGroup,
-      "Date added": member[0].dateAdded,
-      "Time added": member[0].timeAdded,
-    }).then((value) {
-      setState(() {
-        isLoading = false;
-      });
-      callback(true);
+    databaseReference.remove().then((_) {
+      debugPrint("Data deleted successfully!");
     }).catchError((error) {
-      setState(() {
-        isLoading = false;
-      });
-      callback(false);
+      debugPrint("Error deleting data: $error");
     });
   }
 
-  deleteEntry(Function(bool success) callback) async {
-    final databaseReference =
-        database.ref("ddea/members/${userDetails["positionHeld"]}");
+  void moveData(String key, Function(bool success) callback) async {
+    DatabaseReference sourceRef =
+        database.ref("ddea/members/${member[0].positionHeld}/$key");
+    DatabaseReference destinationRef =
+        database.ref("ddea/approved/members/${member[0].positionHeld}/$key");
 
-    //databaseReference.ref.child(path)
-  }
+    // Step 1: Read the Data
+    DataSnapshot dataSnapshot =
+        await sourceRef.once().then((DatabaseEvent event) {
+      return event.snapshot;
+    });
+
+    // Step 2: Write to the New Node
+    destinationRef.set(dataSnapshot.value);
+
+    // Step 3: Delete the Data from the Source Node
+    sourceRef.remove().then((_) {
+      setState(() {
+        isLoadingApproved = false;
+      });
+    });
+  } */
 }
