b3091a26dfbccadfc8ba78caa168cf656c0ca030
Eugene Asiedu <uginasiedu079@gmail.com>
2023-12-21 00:53:35 +0000

    Added he provider and service files

---
+++ lib/helpers/firebase_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '../models/user_model.dart';
+import 'firebase_service.dart';
+
+class FirebaseProvider extends ChangeNotifier {
+  FirebaseService get service => GetIt.I<FirebaseService>();
+  List<UserModel> _allRequests = [];
+  List<UserModel> _allUsers = [];
+  List<UserModel> get allRequests => _allRequests;
+  List<UserModel> get allUsers => _allUsers;
+  bool isLoading = true;
+  bool isLoadingMove = false;
+  bool isLoadingDelete = false;
+  bool isLoadingAllUsers = false;
+
+  FirebaseProvider() {
+    retrieveRequests();
+    retrieveAllUsers();
+  }
+
+  retrieveRequests() async {
+    _allRequests = await service.retrieveRequests();
+    isLoading = false;
+    notifyListeners();
+  }
+
+  retrieveAllUsers() async {
+    _allUsers = await service.retrieveAllUsers();
+    isLoadingAllUsers = false;
+    notifyListeners();
+  }
+
+  moveRequestsToApproved(
+      String node, String key, Function(bool success) callback) {
+    service.moveData(node, key, callback);
+    isLoadingDelete = false;
+    notifyListeners();
+  }
+
+  deleteRequest(String node, String key, Function(bool success) callback) {
+    service.deleteData(node, key, callback);
+    isLoadingDelete = false;
+    notifyListeners();
+  }
+}
---
+++ lib/helpers/firebase_service.dart
@@ -0,0 +1,108 @@
+import 'package:ddea_web/pages/admin/dashboard.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/user_model.dart';
+
+class FirebaseService {
+  final FirebaseDatabase databaseInstance = FirebaseDatabase.instance;
+
+  Future<List<UserModel>> retrieveRequests() async {
+    List subnodePaths = [
+      "Elder (Eld)",
+      "Deacon (Dcn)",
+      "Deaconess (Dcns)",
+      "Member (M)",
+      "Presiding Elder (PE)",
+      "Pastor (Ps)"
+    ];
+
+    final DatabaseReference databaseReference = databaseInstance.ref();
+    List<UserModel> userRequests = [];
+
+    for (int i = 0; i < subnodePaths.length; i++) {
+      String subnode = subnodePaths[i];
+      try {
+        DatabaseEvent event = await databaseReference
+            .child('ddea/members/$subnode')
+            .once(); // Use once() to get a Future
+
+        final dynamic dataSnapshot = event.snapshot.value;
+        if (dataSnapshot != null && dataSnapshot is Map<dynamic, dynamic>) {
+          dataSnapshot.forEach((key, user) {
+            userRequests.add(UserModel.fromSnapshot(event.snapshot.child(key)));
+          });
+        } else {
+          debugPrint('No data found for $subnode');
+        }
+      } catch (error) {
+        debugPrint('Error fetching data for $subnode: $error');
+      }
+    }
+    return userRequests;
+  }
+
+  Future<List<UserModel>> retrieveAllUsers() async {
+    List subnodePaths = [
+      "Elder (Eld)",
+      "Deacon (Dcn)",
+      "Deaconess (Dcns)",
+      "Member (M)",
+      "Presiding Elder (PE)",
+      "Pastor (Ps)"
+    ];
+
+    final DatabaseReference databaseReference = databaseInstance.ref();
+    List<UserModel> allusers = [];
+
+    for (int i = 0; i < subnodePaths.length; i++) {
+      String subnode = subnodePaths[i];
+      try {
+        DatabaseEvent event = await databaseReference
+            .child('ddea/approved/members/$subnode')
+            .once(); // Use once() to get a Future
+
+        final dynamic dataSnapshot = event.snapshot.value;
+        if (dataSnapshot != null && dataSnapshot is Map<dynamic, dynamic>) {
+          dataSnapshot.forEach((key, user) {
+            allusers.add(UserModel.fromSnapshot(event.snapshot.child(key)));
+          });
+        } else {
+          debugPrint('No data found for $subnode');
+        }
+      } catch (error) {
+        debugPrint('Error fetching data for $subnode: $error');
+      }
+    }
+
+    return allusers;
+  }
+
+  void moveData(
+      String node, String key, Function(bool success) callback) async {
+    DatabaseReference sourceRef =
+        databaseInstance.ref("ddea/members/$node/$key");
+    DatabaseReference destinationRef =
+        databaseInstance.ref("ddea/approved/members/$node/$key");
+
+    // Step 1: Read the Data
+    DataSnapshot dataSnapshot =
+        await sourceRef.once().then((DatabaseEvent event) {
+      return event.snapshot;
+    });
+
+    // Step 2: Write to the New Node
+    destinationRef.set(dataSnapshot.value);
+
+    // Step 3: Delete the Data from the Source Node
+    sourceRef.remove();
+  }
+
+  void deleteData(
+      String node, String key, Function(bool success) callback) async {
+    DatabaseReference sourceRef =
+        databaseInstance.ref("ddea/members/$node/$key");
+    sourceRef.remove();
+  }
+}
